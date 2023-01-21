70417a7c689780fdba384b3ae177454465199008
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-01-21 10:53:07 +0530

    Add files via upload

---
+++ Maths/checkPrime.py
@@ -0,0 +1,27 @@
+def isPrime(n):
+    if n==1:
+        return False
+    if n==2 or n==3:
+        return True
+    if n%2==0 or n%3==0:
+        return False
+    i=5
+    while(i*i<=n):
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i+=6
+    return True
+
+def Prime(n):
+    if n==1:
+        return False
+    i=2
+    while(i*i<=n):
+        if n%i==0:
+            return False
+        i+=1
+    return True
+
+n=22
+print(isPrime(n))
+print(Prime(n))
