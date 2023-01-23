d9a4a24e003e4c52683276638179cab95e77aab8
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-01-23 18:11:48 +0530

    Add files via upload

---
+++ List/List_Introduction.py
@@ -0,0 +1,21 @@
+'''
+A ist a collection of items which used array data structure internally.
+Benefit of List is random access of element in constant time.
+It is dynamic in size. 
+'''
+
+#Advantages
+'''
+1. Random Access in constant time.
+2. Cache Friendly(System store the data in cache memory)
+'''
+
+#Disadvantages
+'''
+1. Insertion, Deletion are slow
+2. Search is also slow for unsorted.(For sorted we can use binary search)
+3. Might preallocate extra space for future additions of elements.
+'''
+
+
+#Append and Pop function are O(1) 
