ea36826d234237db9cba4246c57498f394fa80e4
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-01-23 15:59:52 +0530

    Add files via upload

---
+++ Doubly Link List/Doubly Link List.py
@@ -0,0 +1,12 @@
+#Advantages
+'''
+1. Can be traversed in both directions
+2. Delete a given node in O(1)
+3. Insert/Delete before a given Node
+4. Inser Delete both ends in O(1) by maintaining a tail
+'''
+#Disadvantages
+'''
+1) Extra Space for prev
+2) Code becomes more complex
+'''
