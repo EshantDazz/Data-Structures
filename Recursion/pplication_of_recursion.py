c38de8a521e029c1fd84de2d4536b46b072430c2
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-01-23 12:58:33 +0530

    Add files via upload

---
+++ Recursion/pplication_of_recursion.py
@@ -0,0 +1,12 @@
+#Applications of Recursion
+'''
+1. Dynamic Program
+2. Backtracking
+3. Divide and Conquer
+'''
+
+#Many Problrms inherently recursive
+'''
+1. Tower of Hanoi
+2. DFS ttraversal
+'''
