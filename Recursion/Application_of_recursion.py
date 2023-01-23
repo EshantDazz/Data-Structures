515900e518ea9593797c39446b2df30ca3c01be1
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-01-23 13:11:01 +0530

    Add files via upload

---
+++ Recursion/Application_of_recursion.py
@@ -0,0 +1,14 @@
+#Applications of Recursion
+'''
+1. Dynamic Program
+2. Backtracking
+3. Divide and Conquer
+'''
+
+#Many Problrms inherently recursive
+'''
+1. Tower of Hanoi
+2. DFS ttraversal
+'''
+
+#Modern Compilers always like to have tail recursion function
