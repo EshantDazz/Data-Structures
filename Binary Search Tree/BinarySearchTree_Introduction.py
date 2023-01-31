63aa61bd51e486ffcc8f6cd868ca0ec547bf8c19
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-02-01 00:28:53 +0530

    Add files via upload

--- Binary Search Tree/BinarySearchTree_Introduction.py
+++ Binary Search Tree/BinarySearchTree_Introduction.py
@@ -3,3 +3,10 @@
 2. All keys are typically considered distinct
 3. Like a link List, It is a linked Data structure
 '''
+
+
+#Applications of BST and self balancing binary search tree
+'''
+1. To maintain sorted stream of data
+2. To implement doubly ended priority queue
+'''
